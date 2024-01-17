5ba30a900b1ef61f6134943ad559a2bb6528f5f3
Icellyy <154350442+Icellyy@users.noreply.github.com>
2024-01-17 14:23:29 +0200

    Add files via upload

---
+++ Компьютерный практикум № 14 (17.01.2024 г.)/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Компьютерный_практикум___14__17._01._2024_г._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+            //int a = 1;
+            //while (a <= 10) 
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //}
+            //Console.ReadKey();
+
+            //int x = 1;
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+            //while (x <= 10);
+            //Console.ReadLine();
+
+            //int x = 1;
+            //for (int i = 1; x <= 9; x++) ;
+            //{ 
+            //Console.WriteLine(x);
+            //}
+            //Console.ReadKey();
+
+
+            ////Задание 2.Вывести на экран целые числа из интервала от 1 до n(цикл с параметром)
+            //Console.Write("Введите значение n: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+
+            ////Задание 3.Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+            //int x = 0;
+            //while (x < 5)
+            //{
+            //    Console.WriteLine("Привет!");
+            //    x++;
+            //}
+            //Console.ReadLine();
+
+
+            ////Задание 4. С использованием цветового оформления консоли:
+            ////а) вывести на экран горизонтальную строку из 18 символов;
+            ////б) вывести на экран вертикальную строку из 25 символов.
+            //// a) 
+            //Console.ForegroundColor = ConsoleColor.Green; // Устанавливаем цвет текста
+            //Console.Write("".PadLeft(18, '+')); // Горизонтальная строка из 18 символов
+            //Console.WriteLine(); // Переход на новую строку
+            ////b) 
+            //Console.ForegroundColor = ConsoleColor.Blue; // Устанавливаем цвет текста
+            //for (int i = 0; i < 25; i++)
+            //{
+            //    Console.WriteLine("+"); // Вертикальная строка из 25 символов
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+}
